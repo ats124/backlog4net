3524e8891306df174232b71c8147f1b7e7c3d4a5
Atsushi Tanaka <ats124@section-9.tech>
2017-06-24 23:52:33 +0900

    OAuthSupport 追加

---
+++ src/Backlog4net/Auth/OAuthSupport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlog4net.Auth
+{
+    public interface OAuthSupport
+    {
+        void SetOAuthClientId(string clientId, string clientSecret);
+        void SetOAuthRedirectUrl(string redirectUrl);
+        string GetOAuthAuthorizationURL();
+        AccessToken GetOAuthAccessToken(string oauthCode);
+        AccessToken RefreshOAuthAccessToken();
+        event EventHandler<AccessToken> OnAccessTokenRefresh;
+    }
+}

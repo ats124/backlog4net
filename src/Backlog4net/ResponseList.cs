b0e5aaaf952757a91ec3d7ef5dbd5fb77941fb37
Atsushi Tanaka <ats124@section-9.tech>
2017-06-24 15:26:53 +0900

    ResponseList 追加

---
+++ src/Backlog4net/ResponseList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Backlog4net
+{
+    /// <summary>
+    /// The interface for response list.
+    /// </summary>
+    public interface ResponseList<T> : IList<T>
+    {
+    }
+}

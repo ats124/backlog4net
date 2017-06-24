0261486071f9bd8b5cd52162b7fd5b856e23d606
Atsushi Tanaka <ats124@section-9.tech>
2017-06-24 16:35:11 +0900

    Conf周り実装追加

---
+++ src/Backlog4net/Auth/AccessToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlog4net.Auth
+{
+    public class AccessToken
+    {
+        public string Type { get; private set; }
+        public string Token { get; private set; }
+        public long Expires { get; private set; }
+        public string Refresh { get; private set; }
+
+        public AccessToken(string token, long expires, string refresh)
+        {
+            this.Token = token;
+            this.Expires = expires;
+            this.Refresh = refresh;
+        }
+    }
+}
---
+++ src/Backlog4net/Conf/BacklogConfigure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlog4net.Conf
+{
+    using Auth;
+
+    public abstract class BacklogConfigure
+    {
+        public AccessToken AccessToken { get; set; }
+        public string ApiKey { get; set; }
+        public int ConnectionTimeout { get; set; }
+        public int ReadTimeout { get; set; }
+        public abstract string OAuthAuthorizationUrl { get; }
+        public abstract string OAuthAccessTokenUrl { get; }
+        public abstract string RestBaseUrl { get; }
+        public abstract string WebAppBaseUrl { get; }
+    }
+}

0a36f549788235cdd85de79cd35f5c315f362d4d
Atsushi Tanaka <ats124@section-9.tech>
2017-06-25 19:26:21 +0900

    ActivityQueryParams

---
+++ src/Backlog4net/Api/Option/ActivityQueryParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlog4net.Api.Option
+{
+    /// <summary>
+    /// Parameters for activities query.
+    /// </summary>
+    public class ActivityQueryParams : QueryParams
+    {
+        public IList<ActivityType> ActivityType { set => AddNewArrayParamValue(value, x => x.ToString("D"), "activityTypeId"); }
+    }
+}

2d575604f4a1c938d812cfcdb436907bb0a1ef7a
Atsushi Tanaka <ats124@section-9.tech>
2017-06-25 16:14:28 +0900

    デフォルトタイムアウトを設定

--- src/Backlog4net/Conf/BacklogConfigure.cs
+++ src/Backlog4net/Conf/BacklogConfigure.cs
@@ -8,9 +8,11 @@ namespace Backlog4net.Conf
 
     public abstract class BacklogConfigure
     {
+        public const int DEFAULT_TIMUEOUT = 100000;
+
         public AccessToken AccessToken { get; set; }
         public string ApiKey { get; set; }
-        public int Timeout { get; set; }
+        public int Timeout { get; set; } = DEFAULT_TIMUEOUT;
         public abstract string OAuthAuthorizationUrl { get; }
         public abstract string OAuthAccessTokenUrl { get; }
         public abstract string RestBaseUrl { get; }

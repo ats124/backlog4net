2966ad5b46323816c2b43166a0fdd4a3be8ee7d6
Atsushi Tanaka <ats124@section-9.tech>
2017-06-26 21:25:36 +0900

    テストプロジェクト追加

---
+++ src/Backlog4net.Test/ApiOptionsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Backlog4net.Test
+{
+    using Api.Option;
+
+    [TestClass]
+    public class ApiOptionsTest
+    {
+        [TestMethod]
+        public void TestActivityQueryParams()
+        {
+            var param = new ActivityQueryParams()
+            {
+                ActivityType = new[] { ActivityType.IssueCommented, ActivityType.IssueCreated },
+                Count = 10,
+                MinId = 1,
+                MaxId = 100,
+                Order = QueryOrder.Asc,
+            };
+
+            var parameters = param.Parameters;
+            Assert.IsTrue(parameters.Any(x => x.Name == "activityTypeId[]" && x.Value == "3"));
+            Assert.IsTrue(parameters.Any(x => x.Name == "activityTypeId[]" && x.Value == "1"));
+            Assert.IsTrue(parameters.Any(x => x.Name == "count" && x.Value == "10"));
+            Assert.IsTrue(parameters.Any(x => x.Name == "minId" && x.Value == "1"));
+            Assert.IsTrue(parameters.Any(x => x.Name == "maxId" && x.Value == "100"));
+            Assert.IsTrue(parameters.Any(x => x.Name == "order" && x.Value == "asc"));
+        }
+    }
+}

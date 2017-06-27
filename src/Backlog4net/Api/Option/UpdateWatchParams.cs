555b8749608455ad07b2bf905f6125e43f1eb4ff
Atsushi Tanaka <ats124@section-9.tech>
2017-06-27 11:31:52 +0900

    いくつからのParamsを追加

---
+++ src/Backlog4net/Api/Option/UpdateWatchParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlog4net.Api.Option
+{
+    public class UpdateWatchParams : PatchParams
+    {
+        private object watchingId;
+
+        public UpdateWatchParams(object watchingId)
+        {
+            this.watchingId = watchingId;
+        }
+
+        public string WatchingIdString => watchingId.ToString();
+
+        public string Note { set => AddNewParamValue(value); }
+    }
+}
---
+++ src/Backlog4net/Api/Option/UpdateWebhookParams.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlog4net.Api.Option
+{
+    public class UpdateWebhookParams : PatchParams
+    {
+        private object projectIdOrKey;
+        private object webhookId;
+
+        public UpdateWebhookParams(object projectIdOrKey, long webhookId)
+        {
+            this.projectIdOrKey = projectIdOrKey;
+            this.webhookId = webhookId;
+        }
+
+        public string ProjectIdOrKeyString => projectIdOrKey.ToString();
+
+        public string WebhookId => webhookId.ToString();
+
+        public string Name { set => AddNewParamValue(value); }
+
+        public string HookUrl { set => AddNewParamValue(value); }
+
+        public string Description { set => AddNewParamValue(value); }
+
+        public bool AllEvent { set => AddNewParamValue(value); }
+
+        public IList<ActivityType> ActivityTypeIds { set => AddNewArrayParamValues(value, x => x.ToString("D")); }
+    }
+}

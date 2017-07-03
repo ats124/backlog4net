815a18150a0d2c02047f5c05951496d8845b28f0
Atsushi Tanaka <ats124@section-9.tech>
2017-07-03 23:04:16 +0900

    テストプロジェクトの下準備

---
+++ src/Backlog4net.Test/IssueMethodsTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Backlog4net.Test
+{
+    using Api;
+    using Api.Option;
+    using Conf;
+    using TestConfig;
+
+    [TestClass]
+    public class IssueMethodsTest
+    {        
+        private static BacklogClient client;
+        private static GeneralConfig generalConfig;
+
+        [ClassInitialize]
+        public static void SetupClient(TestContext context)
+        {
+            generalConfig = GeneralConfig.Instance.Value;
+            var conf = new BacklogJpConfigure(generalConfig.SpaceKey);
+            conf.ApiKey = generalConfig.ApiKey;
+            client = new BacklogClientFactory(conf).NewClient();
+        }
+
+        [TestMethod]
+        public async Task GetIssuesCountAsync()
+        {
+            var count = await client.GetIssuesCountAsync(new GetIssuesCountParams(55947));
+            Assert.IsTrue(count > 0);
+        }
+    }
+}
---
+++ src/Backlog4net.Test/ProjectMethodsTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Backlog4net.Test
+{
+    using Api;
+    using Api.Option;
+    using Conf;
+    using TestConfig;
+
+    [TestClass]
+    public class ProjectMethodsTest
+    {
+        private static BacklogClient client;
+        private static GeneralConfig generalConfig;
+
+        [ClassInitialize]
+        public static void SetupClient(TestContext context)
+        {
+            generalConfig = GeneralConfig.Instance.Value;
+            var conf = new BacklogJpConfigure(generalConfig.SpaceKey);
+            conf.ApiKey = generalConfig.ApiKey;
+            client = new BacklogClientFactory(conf).NewClient();
+        }
+
+        [TestMethod]
+        public async Task GetProjectsAsyncTest()
+        {
+            var projects = await client.GetProjectsAsync();
+            Assert.IsNotNull(projects);
+            Assert.IsTrue(projects.Count > 0);
+            Assert.IsFalse(string.IsNullOrEmpty(projects[0].Name));
+        }
+    }
+}

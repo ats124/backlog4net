c8246c45b946116f13ccee1d95964dca21049718
Atsushi Tanaka <ats124@section-9.tech>
2017-07-28 22:11:18 +0900

    Add IssuesConfig

---
+++ src/Backlog4net.Test/TestConfig/IssuesConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Backlog4net.Test.TestConfig
+{
+
+    public class IssuesConfig
+    {
+        public static Lazy<IssuesConfig> Instance { get; } = new Lazy<IssuesConfig>(() => JsonConvert.DeserializeObject<IssuesConfig>(File.ReadAllText(@"TestConfig\issues.json")));
+
+        [JsonProperty]
+        public string NotifiedUserId1 { get; private set; }
+
+        [JsonProperty]
+        public string NotifiedUserId2 { get; private set; }
+
+        [JsonProperty]
+        public string NotifiedUserId3 { get; private set; }
+
+    }
+
+}

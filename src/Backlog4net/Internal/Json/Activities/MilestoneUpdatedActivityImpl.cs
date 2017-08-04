5eaf3b394d8df1a4f76d0a07076301c1f4e31d03
Atsushi Tanaka <ats124@section-9.tech>
2017-08-04 14:36:07 +0900

    マイルストーンの追加、更新、削除のActivity実装クラスを追加する #67

---
+++ src/Backlog4net/Internal/Json/Activities/MilestoneUpdatedActivityImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Backlog4net.Internal.Json.Activities
+{
+    public class MilestoneUpdatedActivityImpl : ActivityJsonImpl<MilestoneUpdatedContentImpl>, MilestoneUpdatedActivity
+    {
+        public override ActivityType Type => ActivityType.MilestoneUpdated;
+
+        MilestoneUpdatedContent MilestoneUpdatedActivity.Content => this.Content;
+    }
+
+    public class MilestoneUpdatedContentImpl : MilestoneUpdatedContent
+    {
+        [JsonProperty]
+        public long Id { get; private set; }
+
+        [JsonProperty()]
+        public string Name { get; private set; }
+
+        [JsonProperty(ItemConverterType = typeof(ChangeJsonImpl.JsonConverter))]
+        public IList<Change> Changes { get; private set; }
+    }
+}

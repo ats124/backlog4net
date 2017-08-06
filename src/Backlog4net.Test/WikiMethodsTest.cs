d216833e635fb6ec1362d999859d73e1552afe78
Atsushi Tanaka <ats124@section-9.tech>
2017-08-06 16:27:17 +0900

    Add WikiTest

--- src/Backlog4net.Test/WikiMethodsTest.cs
+++ src/Backlog4net.Test/WikiMethodsTest.cs
@@ -22,6 +22,8 @@ namespace Backlog4net.Test
     {
         private static BacklogClient client;
         private static GeneralConfig generalConfig;
+        private static string projectKey;
+        private static long projectId;
 
         [ClassInitialize]
         public static async Task SetupClient(TestContext context)
@@ -31,6 +33,45 @@ namespace Backlog4net.Test
             conf.ApiKey = generalConfig.ApiKey;
             client = new BacklogClientFactory(conf).NewClient();
             var users = await client.GetUsersAsync();
+
+            projectKey = generalConfig.ProjectKey;
+            var project = await client.GetProjectAsync(projectKey);
+            projectId = project.Id;
         }
+
+        [TestMethod]
+        public async Task WikiTest()
+        {
+            var wiki = await client.CreateWikiAsync(new CreateWikiParams(projectId, "TestName", "TestContent")
+            {
+                MailNotify = false,
+            });
+            Assert.AreEqual(wiki.ProjectId, projectId);
+            Assert.AreNotEqual(wiki.Id, 0);
+            Assert.AreEqual(wiki.Name, "TestName");
+            Assert.AreEqual(wiki.Content, "TestContent");
+
+            var wikiGet = await client.GetWikiAsync(wiki.Id);
+            Assert.AreEqual(wikiGet.Id, wiki.Id);
+            Assert.AreEqual(wikiGet.Name, "TestName");
+            Assert.AreEqual(wikiGet.Content, "TestContent");
+
+            var wikiUpdated = await client.UpdateWikiAsync(new UpdateWikiParams(wiki.Id)
+            {
+                Name = "TestNameUpdated",
+                Content = "TestContentUpdated",                
+                MailNotify = false,
+            });
+            Assert.AreEqual(wikiUpdated.Id, wiki.Id);
+            Assert.AreEqual(wikiUpdated.Name, "TestNameUpdated");
+            Assert.AreEqual(wikiUpdated.Content, "TestContentUpdated");
+
+            var wikiDeleted = await client.DeleteWikiAsync(wiki.Id, false);
+            Assert.AreEqual(wikiDeleted.Id, wiki.Id);
+            Assert.AreEqual(wikiDeleted.Name, "TestNameUpdated");
+            Assert.AreEqual(wikiDeleted.Content, "TestContentUpdated");
+        }
+
+
     }
 }

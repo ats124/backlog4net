89d2acde0bf90186768ec5b66086816fcea55ab3
Atsushi Tanaka <ats124@section-9.tech>
2017-08-19 15:17:17 +0900

    Add UserMethodsTest

---
+++ src/Backlog4net.Test/UserMethodsTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Backlog4net.Test
+{
+    using Api;
+    using Api.Option;
+    using Backlog4net.Internal.Json;
+    using Backlog4net.Internal.Json.Activities;
+    using Conf;
+    using Newtonsoft.Json;
+    using TestConfig;
+
+    [TestClass]
+    public class UserMethodsTest
+    {
+        private static BacklogClient client;
+        private static GeneralConfig generalConfig;
+        private static string projectKey;
+        private static long projectId;
+
+        [ClassInitialize]
+        public static async Task SetupClient(TestContext context)
+        {
+            generalConfig = GeneralConfig.Instance.Value;
+            var conf = new BacklogJpConfigure(generalConfig.SpaceKey);
+            conf.ApiKey = generalConfig.ApiKey;
+            client = new BacklogClientFactory(conf).NewClient();
+            var users = await client.GetUsersAsync();
+
+            projectKey = generalConfig.ProjectKey;
+            var project = await client.GetProjectAsync(projectKey);
+            projectId = project.Id;
+        }
+    }
+}

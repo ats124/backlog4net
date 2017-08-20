59e07b6a0d7ffbef6286116a12a605c1b286317c
Atsushi Tanaka <ats124@section-9.tech>
2017-08-20 18:03:18 +0900

    GetUserWatchCountAsync, GetUserWatchesAsync 引数見直し #80

---
+++ src/Backlog4net/Api/Option/GetWatchCountParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlog4net.Api.Option
+{
+    public class GetWatchCountParams : GetParams
+    {
+        public bool AlreadyRead { set => AddNewParamValue(value); }
+
+        public bool ResourceAlreadyRead { set => AddNewParamValue(value); }
+    }
+}

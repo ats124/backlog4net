9f9f6a2ed6ac7c25c67095c86f912c08fd86f423
Atsushi Tanaka <ats124@section-9.tech>
2017-08-22 22:47:25 +0900

    Add ImportMethodsTest class

---
+++ src/Backlog4net.Test/ImportMethodsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Backlog4net.Test
+{
+    using Api;
+    using Api.Option;
+    using Backlog4net.Internal.Json;
+    using Backlog4net.Internal.Json.Activities;
+    using Conf;
+    using Newtonsoft.Json;
+    using TestConfig;
+
+    [TestClass]
+    public class ImportMethodsTest
+    {
+        private static BacklogClient client;
+        private static GeneralConfig generalConfig;
+        private static string projectKey;
+        private static long projectId;
+        private static User anotherUser;
+
+        [ClassInitialize]
+        public static async Task SetupClient(TestContext context)
+        {
+            generalConfig = GeneralConfig.Instance.Value;
+            var conf = new BacklogJpConfigure(generalConfig.SpaceKey);
+            conf.ApiKey = generalConfig.ApiKey;
+            client = new BacklogClientFactory(conf).NewClient();
+            var users = await client.GetUsersAsync();
+
+            projectKey = generalConfig.ProjectKey;
+            var project = await client.GetProjectAsync(projectKey);
+            projectId = project.Id;
+
+            var conf2 = new BacklogJpConfigure(generalConfig.SpaceKey);
+            conf2.ApiKey = generalConfig.ApiKey2;
+            var client2 = new BacklogClientFactory(conf).NewClient();
+            anotherUser = await client2.GetMyselfAsync();
+        }
+    }
+}

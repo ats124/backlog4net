a4fddf77df0cae50013e06630323e6281925b116
Atsushi Tanaka <ats124@section-9.tech>
2017-08-22 23:43:30 +0900

    Add GroupTestAsync

--- src/Backlog4net.Test/GroupMethodsTest.cs
+++ src/Backlog4net.Test/GroupMethodsTest.cs
@@ -25,6 +25,7 @@ namespace Backlog4net.Test
         private static string projectKey;
         private static long projectId;
         private static User ownUser;
+        private static User anotherUser;
 
         [ClassInitialize]
         public static async Task SetupClient(TestContext context)
@@ -40,6 +41,44 @@ namespace Backlog4net.Test
             projectId = project.Id;
 
             ownUser = await client.GetMyselfAsync();
+
+            var conf2 = new BacklogJpConfigure(generalConfig.SpaceKey);
+            conf2.ApiKey = generalConfig.ApiKey2;
+            var client2 = new BacklogClientFactory(conf).NewClient();
+            anotherUser = await client2.GetMyselfAsync();
+        }
+
+        [TestMethod]
+        public async Task GroupTestAsync()
+        {
+            var group = await client.CreateGroupAsync(new CreateGroupParams("TestGroup")
+            {
+                Members = new[] { ownUser.Id }
+            });
+            Assert.AreNotEqual(group.Id, 0L);
+            Assert.AreEqual(group.Name, "TestGroup");
+            Assert.IsTrue(group.Members.Any(x => x.Id == ownUser.Id));
+
+            var groupGet = await client.GetGroupAsync(group.Id);
+            Assert.AreEqual(groupGet.Id, group.Id);
+            Assert.AreEqual(groupGet.Name, group.Name);
+            Assert.IsTrue(groupGet.Members.Any(x => x.Id == ownUser.Id));
+
+            var groupUpdated = await client.UpdateGroupAsync(new UpdateGroupParams(group.Id)
+            {
+                Name = "TestGroupUpdated",
+                Members = new[] { anotherUser.Id }
+            });
+            Assert.AreEqual(groupUpdated.Id, group.Id);
+            Assert.AreEqual(groupUpdated.Name, "TestGroupUpdated");
+            Assert.IsTrue(groupGet.Members.Any(x => x.Id == anotherUser.Id));
+
+            await client.GetGroupsAsync(new OffsetParams() { Count = 100, Offset = 1, Order = Order.Desc });
+
+            var groupDeleted = await client.DeleteGroupAsync(groupUpdated.Id);
+            Assert.AreEqual(groupDeleted.Id, groupDeleted.Id);
+            Assert.AreEqual(groupDeleted.Name, groupDeleted.Name);
+            Assert.IsTrue(groupDeleted.Members.Any(x => x.Id == anotherUser.Id));
         }
     }
 }

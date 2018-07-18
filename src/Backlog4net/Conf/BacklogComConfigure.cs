4b8869f443e950740a335f40006d009a88569f5d
Atsushi Tanaka <ats124@section-9.tech>
2018-07-18 23:28:10 +0900

    add: BacklogComConfigure

---
+++ src/Backlog4net/Conf/BacklogComConfigure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlog4net.Conf
+{
+    public sealed class BacklogComConfigure : BacklogConfigure
+    {
+        private readonly string spaceKey;
+
+        public BacklogComConfigure(string spaceKey)
+        {
+            if (spaceKey == null) throw new ArgumentNullException(nameof(spaceKey));
+            this.spaceKey = spaceKey;
+        }
+
+        public override string OAuthAuthorizationUrl => $"https://{spaceKey}.backlog.com/OAuth2AccessRequest.action";
+
+        public override string OAuthAccessTokenUrl => $"https://{spaceKey}.backlog.com/api/v2/oauth2/token";
+
+        public override string RestBaseUrl => $"https://{spaceKey}.backlog.com/api/v2";
+
+        public override string WebAppBaseUrl => $"https://{spaceKey}.backlog.com";
+    }
+}
